2f5a9d23c39cf82cca62ff2a4e657ef456c442f9
stanwys <44429330+stanwys@users.noreply.github.com>
2020-11-25 10:27:21 +0100

    update

--- app/src/main/java/com/example/footballapp/MyDBHandler.kt
+++ app/src/main/java/com/example/footballapp/MyDBHandler.kt
@@ -24,7 +24,6 @@ import java.nio.file.Files.getLastModifiedTime
 import java.util.*
 import javax.xml.parsers.DocumentBuilderFactory
 import kotlin.collections.ArrayList
-//import com.example.bricklist.InventoryShow.*
 import org.w3c.dom.Element
 import org.w3c.dom.Node
 import org.w3c.dom.NodeList
